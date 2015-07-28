5764941dc53857d8784670b198a643a4909bb9dd
Carl Winkler <carl@carlossus.com>
2015-07-28 13:41:23 +0800

    Added toRow/toAsk modules to repo

--- src/errors.ts
+++ src/errors.ts
@@ -2,5 +2,6 @@ export = errors;
 
 var errors = {
 	FunctionExists: "The functionId already exists on that topic",
-	NoHandler: "This is no handler available for that filter and functionId"
+	NoHandler: "This is no handler available for that filter and functionId",
+	MustSupplyDbName: "No database name was provided" 
 }
