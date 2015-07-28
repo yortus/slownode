9dc10a60b920ff8cf1c022d16794b3523b3b7800
Carl Winkler <carl@carlossus.com>
2015-07-28 12:47:21 +0800

    Added errors module

---
+++ src/errors.js
@@ -0,0 +1,5 @@
+var errors = {
+    FunctionExists: "The functionId already exists on that topic"
+};
+module.exports = errors;
+//# sourceMappingURL=errors.js.map
---
+++ src/errors.ts
@@ -0,0 +1,5 @@
+export = errors;
+
+var errors = {
+	FunctionExists: "The functionId already exists on that topic"
+}
