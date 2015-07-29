6e4f5599fe98d753d5d260170c993a6078c7bea0
Carl Winkler <carl@carlossus.com>
2015-07-29 13:32:53 +0800

    Remove subscribers.get module due to deprecation

--- src/subscribers/get.js
+++
@@ -1,8 +0,0 @@
-var _this = this;
-var get = function (topicFilter, functionId) {
-    var self = _this;
-    var topicHandlers = self.subscribers[topicFilter] || {};
-    return topicHandlers[functionId] || null;
-};
-module.exports = get;
-//# sourceMappingURL=get.js.map
--- src/subscribers/get.ts
+++
@@ -1,9 +0,0 @@
-import Types = require("event-loop");
-export = get;
-
-var get = (topicFilter: string, functionId: string) => {
-	var self: Types.EventLoop = this;
-	
-	var topicHandlers = self.subscribers[topicFilter] || {};
-	return topicHandlers[functionId] || null;
-}
