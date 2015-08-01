ca9677a7d7becc52c4fd9461bc419fd90754da52
Carl Winkler <carl.winkler86@gmail.com>
2015-08-01 23:06:37 +0800

    Refactor exit module
    Will delete the database if it exists or return true if it already doesn't exist

--- src/exit.js
+++ src/exit.js
@@ -2,7 +2,19 @@ var Promise = require("bluebird");
 var fs = require("fs");
 var db = require("./store/db");
 var unlink = Promise.promisify(fs.unlink);
+var readFile = Promise.promisify(fs.readFile);
 function exit() {
+    return databaseExists()
+        .then(teardown);
+}
+function databaseExists() {
+    return readFile("slownode.db")
+        .then(function () { return true; })
+        .catch(function () { return false; });
+}
+function teardown(exists) {
+    if (exists)
+        return Promise.resolve(true);
     return db.destroy()
         .then(function () { return unlink("slownode.db"); })
         .then(function () { return true; })
--- src/exit.ts
+++ src/exit.ts
@@ -2,9 +2,23 @@ import Promise = require("bluebird")
 import fs = require("fs");
 import db = require("./store/db");
 var unlink = Promise.promisify(fs.unlink);
+var readFile = Promise.promisify(fs.readFile);
 export = exit;
 
 function exit() {
+	return databaseExists()
+		.then(teardown);
+}
+
+function databaseExists() {
+	return readFile("slownode.db")
+		.then(() => true)
+		.catch(() => false);
+}
+
+function teardown(exists: boolean) {
+	if (exists) return Promise.resolve(true);
+
 	return db.destroy()
 		.then(() => unlink("slownode.db"))
 		.then(() => true)
