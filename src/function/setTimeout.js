9a3d76534a6c11119dfaac967ba98d044eda2434
Carl Winkler <carl@carlossus.com>
2015-08-03 10:52:16 +0800

    Implement setTimeout logic

--- src/function/setTimeout.js
+++ src/function/setTimeout.js
@@ -1,11 +1,11 @@
-var store = require("../store/eventLoop");
-var serialise = require("./serialise");
-function timeout(func, delayMs) {
-    var serialisedFunc = serialise(func);
+var store = require("../store/function");
+function timeout(func, delayMs, options) {
+    options = options || {};
+    options.intervalMs = 0;
+    options.runAt = Date.now() + delayMs;
     return store.add({
-        arguments: "[]",
-        functionId: serialisedFunc,
-        runAt: Date.now() + delayMs,
+        body: func,
+        options: options
     });
 }
 module.exports = timeout;
--- src/function/setTimeout.ts
+++ src/function/setTimeout.ts
@@ -1,14 +1,14 @@
-import SlowNode = require("slownode");
-import store = require("../store/eventLoop");
-import serialise = require("./serialise");
+import Types = require("slownode");
+import store = require("../store/function");
 export = timeout;
 
-function timeout(func: () => any, delayMs: number) {
-	var serialisedFunc = serialise(func);
+function timeout(func: () => any, delayMs: number, options?: Types.SlowFunctionOptions) {
+	options = options || {};
+	options.intervalMs = 0;
+	options.runAt = Date.now() + delayMs;
 	
 	return store.add({
-		arguments: "[]",
-		functionId: serialisedFunc,
-		runAt: Date.now() + delayMs,
+		body: func,
+		options: options
 	});
 }
