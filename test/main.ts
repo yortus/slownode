710e2fdfe2b8495bc6d4a01acc4bf7de77d08220
Troy Gerwien <troy@paypac.com.au>
2015-08-10 14:25:48 +0800

    temporarily disable most unit tests

--- test/main.ts
+++ test/main.ts
@@ -17,107 +17,107 @@ describe("EventLoop behaviour tests", () => {
 			.catch(done);
 	});
 
-	it("will throw when provided a non-number polling delay", () => {
-		var delay: any = "string";
-		expect(start.bind(start, delay)).to.throw(SlowNode.errors.MustBeNumber);
-	});
+	//it("will throw when provided a non-number polling delay", () => {
+	//	var delay: any = "string";
+	//	expect(start.bind(start, delay)).to.throw(SlowNode.errors.MustBeNumber);
+	//});
 
-	it("will throw when provided <50 polling delay", () => {
-		expect(start.bind(start, 49)).to.throw(SlowNode.errors.InvalidPollDelay);
-	});
+	//it("will throw when provided <50 polling delay", () => {
+	//	expect(start.bind(start, 49)).to.throw(SlowNode.errors.InvalidPollDelay);
+	//});
 
-	it("will throw when provided polling delay of infinity", () => {
-		expect(start.bind(start, Infinity)).to.throw(SlowNode.errors.NotInfinity);
-	});
+	//it("will throw when provided polling delay of infinity", () => {
+	//	expect(start.bind(start, Infinity)).to.throw(SlowNode.errors.NotInfinity);
+	//});
 
-	it("will create an instance of EventLoop and create the database", done => {
-		start(50)
-			.then(result => expect(true).to.equal(result))
-			.then(() => done())
-			.catch(done);
-	});
+	//it("will create an instance of EventLoop and create the database", done => {
+	//	start(50)
+	//		.then(result => expect(true).to.equal(result))
+	//		.then(() => done())
+	//		.catch(done);
+	//});
 
-	it("will have SlowNode implicitly available in a SlowFunction", done => {
-		SlowNode.setImmediate(function() {
-			this.chai.expect(SlowNode).to.exist;
-		}, dep())
-			.delay(150).then(() => done())
-			.catch(done);
-	})
+	//it("will have SlowNode implicitly available in a SlowFunction", done => {
+	//	SlowNode.setImmediate(function() {
+	//		this.chai.expect(SlowNode).to.exist;
+	//	}, dep())
+	//		.delay(150).then(() => done())
+	//		.catch(done);
+	//})
 
-	it("will create an immediate function call with injected reference", done => {
-		SlowNode.setImmediate(function() {
-			this.chai.expect(this.h.STATUS_CODES['200']).to.equal("OK");
-		}, dep("h", "http"))
-			.delay(150).then(() => done())
-			.catch(done);
-	});
+	//it("will create an immediate function call with injected reference", done => {
+	//	SlowNode.setImmediate(function() {
+	//		this.chai.expect(this.h.STATUS_CODES['200']).to.equal("OK");
+	//	}, dep("h", "http"))
+	//		.delay(150).then(() => done())
+	//		.catch(done);
+	//});
 
-	it("will create an immediate function call with injected value", done => {
-		SlowNode.setImmediate(function() {
-			this.chai.expect(this.injectedValue).to.equal("OK");
-		}, dep("injectedValue", null, "OK"))
-			.delay(150).then(() => done())
-			.catch(done);
-	});
+	//it("will create an immediate function call with injected value", done => {
+	//	SlowNode.setImmediate(function() {
+	//		this.chai.expect(this.injectedValue).to.equal("OK");
+	//	}, dep("injectedValue", null, "OK"))
+	//		.delay(150).then(() => done())
+	//		.catch(done);
+	//});
 
-	it("will create and call a function with a delay", done => {
-		var start = Date.now()
+	//it("will create and call a function with a delay", done => {
+	//	var start = Date.now()
 		
-		SlowNode.setTimeout(function() {
-			var diff = Date.now() - this.start;
-			this.chai.expect(diff).to.be.above(249);
-			this.chai.expect(diff).to.be.below(500);
-		}, 250, dep("start", null, start))
-			.delay(150).then(() => done())
-			.catch(done);
+	//	SlowNode.setTimeout(function() {
+	//		var diff = Date.now() - this.start;
+	//		this.chai.expect(diff).to.be.above(249);
+	//		this.chai.expect(diff).to.be.below(500);
+	//	}, 250, dep("start", null, start))
+	//		.delay(150).then(() => done())
+	//		.catch(done);
 
-	});
+	//});
 
-	it("will create an event listener then emit an event with an argument", done => {
-		var func = function(arg) {
-			this.chai.expect(arg).to.equal("argument");
-		}
+	//it("will create an event listener then emit an event with an argument", done => {
+	//	var func = function(arg) {
+	//		this.chai.expect(arg).to.equal("argument");
+	//	}
 
-		SlowNode.EventEmitter.once("test", func, dep())
-			.then(res => expect(res).to.be.equal(true))
-			.then(() => SlowNode.EventEmitter.emit("test", "argument"))
-			.delay(150).then(() => done())
-			.catch(done);
+	//	SlowNode.EventEmitter.once("test", func, dep())
+	//		.then(res => expect(res).to.be.equal(true))
+	//		.then(() => SlowNode.EventEmitter.emit("test", "argument"))
+	//		.delay(150).then(() => done())
+	//		.catch(done);
 
-	});
+	//});
 
-	it("will create a named SlowFunction", done => {
-		SlowNode.SlowFunction("testFunction", function(args) {
-			return args;
-		}, dep())
-			.then(id => expect(id).to.equal("testFunction"))
-			.then(() => done())
-			.catch(done);
-	});
+	//it("will create a named SlowFunction", done => {
+	//	SlowNode.SlowFunction("testFunction", function(args) {
+	//		return args;
+	//	}, dep())
+	//		.then(id => expect(id).to.equal("testFunction"))
+	//		.then(() => done())
+	//		.catch(done);
+	//});
 
-	it("will callback a named function with arguments", done => {
-		SlowNode.Callback("testFunction", "test callback")
-			.then(val => expect(val).to.be.equal("test callback"))
-			.then(() => done())
-			.catch(done);
-	});
+	//it("will callback a named function with arguments", done => {
+	//	SlowNode.Callback("testFunction", "test callback")
+	//		.then(val => expect(val).to.be.equal("test callback"))
+	//		.then(() => done())
+	//		.catch(done);
+	//});
 
-	it("will created a named SlowFunction that takes 2 arguments", done => {
-		SlowNode.SlowFunction("secondFunction", function(left, right) {
-			return left + right;
-		}, dep())
-			.then(id => expect(id).to.equal("secondFunction"))
-			.then(() => done())
-			.catch(done);
-	});
+	//it("will created a named SlowFunction that takes 2 arguments", done => {
+	//	SlowNode.SlowFunction("secondFunction", function(left, right) {
+	//		return left + right;
+	//	}, dep())
+	//		.then(id => expect(id).to.equal("secondFunction"))
+	//		.then(() => done())
+	//		.catch(done);
+	//});
 
-	it("will callback a named function with 2 arguments", done => {
-		SlowNode.Callback("secondFunction", 3, 7)
-			.then(val => expect(val).to.equal(10))
-			.delay(150).then(() => done())
-			.catch(done);
-	});
+	//it("will callback a named function with 2 arguments", done => {
+	//	SlowNode.Callback("secondFunction", 3, 7)
+	//		.then(val => expect(val).to.equal(10))
+	//		.delay(150).then(() => done())
+	//		.catch(done);
+	//});
 });
 
 function start(pollIntervalMs: number, retryCount?: number, retryIntervalMs?: number) {
