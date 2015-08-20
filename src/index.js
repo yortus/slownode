ac41d9d90093ab11c51356f21ff52de2d5b2be94
Carl Winkler <carl@carlossus.com>
2015-08-20 16:01:37 +0800

    Convert existsSync call to statSync due to deprecation notice

--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ var async = require('./slowAsyncFunction/slowAsyncFunction');
 var rehydrate = require('./slowAsyncFunction/rehydrate');
 // Resume the current epoch (if DB exists) or start a new epoch (if no DB).
 // NB: Module initialisation must be synchronous, so we use only sync methods here.
-if (!fs.existsSync(databaseLocation)) {
-    // Start a new epoch by copying from the empty template database (synchronously).
+try {
+    fs.statSync(databaseLocation);
+}
+catch (ex) {
     var templateLocation = path.join(__dirname, '../empty.db');
     fs.writeFileSync(databaseLocation, fs.readFileSync(templateLocation));
 }
