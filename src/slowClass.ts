fcde02e44cd64da3472e94e3b4fe78a9ecb2d258
Troy Gerwien <yortus@gmail.com>
2015-10-28 09:37:28 +0800

    SlowClass implements SlowObject

--- src/slowClass.ts
+++ src/slowClass.ts
@@ -3,9 +3,11 @@ import SlowLog = require('./slowLog');
 export = SlowClass;
 
 
-declare class SlowClass {
+declare class SlowClass implements SlowObject {
 
     constructor(...args);
+
+    $slow: any;
 
     $slowLog: SlowLog;
 
