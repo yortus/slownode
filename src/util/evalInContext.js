9258e69cf2c6e289b2be89fc180f7675de6673d0
Troy Gerwien <yortus@gmail.com>
2015-11-19 15:23:08 +0800

    remove dead code

--- src/util/evalInContext.js
+++
@@ -1,9 +0,0 @@
-function evalInContext(source, context) {
-    // TODO: this won't work in strict mode. Will need to do it another way eventually (ie using 'eval' but not 'with')...
-    // TODO: use 'vm' module
-    var result;
-    eval("with (context) result = (" + source + ");");
-    return result;
-}
-module.exports = evalInContext;
-//# sourceMappingURL=evalInContext.js.map
