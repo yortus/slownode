ed909ef20a6fd96da10573ce4605f305c788a34b
Troy Gerwien <yortus@gmail.com>
2016-05-16 15:48:07 +0800

    refactor around 'Tasks' WIP II

--- src/create-task.ts
+++ src/create-task.ts
@@ -1,9 +1,8 @@
 'use strict';
 import * as babel from 'babel';
-import {Node} from "babel-types";                   // Elided (used only for types)
+import {Node, File} from "babel-types";             // Elided (used only for types)
 import {Visitor, Binding} from "babel-traverse";    // Elided (used only for types)
 import IL from './il';
-import {IdentifierList} from './scope';
 import transformToIL from './transform-to-il';
 import Task from './task';
 import CreateTaskOptions from './create-task-options';
@@ -12,20 +11,11 @@ import CreateTaskOptions from './create-task-options';
 
 
 
-// TODO: augment...
+// TODO: augment nodes with scope info...
 declare module 'babel-types' {
     interface Node {
         scope?: ScopeInfo;
     }
-}
-
-
-
-
-
-// TODO: ...
-export default function createTask(code: string, options: CreateTaskOptions): Task {
-    return <any> transform(code);
 }
 
 
@@ -39,91 +29,48 @@ export type ScopeInfo = {[name: string]: 'var'|'let'|'const'|'hoisted'|'param'|'
 
 
 
-// // TODO: doc... define a babel plugin that collects all scope info
-// function makeScopeInfoPlugin(scopeInfo: ScopeInfo) {
-//     return (b: typeof babel) => ({
-//         visitor: <Visitor> {
-//             // Collect info for all block-scopes that have their own bindings.
-//             // TODO: What introduces a new name?
-//             // - var decl (var, let, const)
-//             // - func decl (hoisted)
-//             // - class decl (not hoisted)
-//             // - import decl (hoisted)
-//             // - func expr. scope of name is only *inside* the func body
-//             // - class expr. scope of name is only *inside* the class body
-//             // - catch clause. scope of name is only the catch clause body
-//             Block(path) {
-//                 if (path.scope.block !== path.node) return;
-//                 let idNames = Object.keys(path.scope.bindings);
-//                 let idKinds = idNames.map(name => path.scope.bindings[name].kind);
-//                 let scope = idNames.reduce((ids, name, i) => (ids[name] = idKinds[i], ids), {});
-//                 scopeInfo.set(path.node, scope);
-//             },
-//         }    
-//     });
-// }
+// TODO: ...
+export default function createTask(code: string, options: CreateTaskOptions): Task {
 
+    // TODO: Get the AST...
+    let plugins = [
+        'transform-es2015-destructuring',
+        augmentNodesWithScopeInfo,
+        (babel) => (b = babel, {})
+    ];
+    let b: typeof babel; // TODO: hack!
+    let ast = <File> babel.transform(code, {plugins}).ast;
 
-
-
-
-// TODO: was......
-
-
-
-
-
-// Export the plugin factory function
-function transform(source: string): string {
-    let il = new IL(source);
-    
-    // Define a babel plugin.
-    let babelPlugin = (b: typeof babel) => {
-        let t = b.types;
-        return {
-            visitor: <Visitor> {
-
-                // Collect info for all block-scopes that have their own bindings.
-                // TODO: What introduces a new name?
-                // - var decl (var, let, const)
-                // - func decl (hoisted)
-                // - class decl (not hoisted)
-                // - import decl (hoisted)
-                // - func expr. scope of name is only *inside* the func body
-                // - class expr. scope of name is only *inside* the class body
-                // - catch clause. scope of name is only the catch clause body
-                Block(path) {
-                    // TODO: disable for now... destructuring plugin makes this assertion fail... why? investigate... 
-                    // TODO: was...assert(path.scope.block === path.node);
-                    // TODO: temp replacement for above assertion:
-                    if (path.scope.block !== path.node) return;
-
-                    let idNames = Object.keys(path.scope.bindings);
-                    let idKinds = idNames.map(name => path.scope.bindings[name].kind);
-                    let scope = idNames.reduce((ids, name, i) => (ids[name] = idKinds[i], ids), <IdentifierList> {});
-                    path.node.scope = scope;
-                },
-
-                // Transform the program.
-                Program: {
-                    exit(path) {
-                        transformToIL(b, path.node, il);
-                        let newSrc = il.compile();
-                        result = newSrc;
-                    }
-                }
-            }
-        };
-    }
-
-
-    // TODO: doc...
-    let result: string;
-    babel.transform(source, {
-        plugins: [
-            'transform-es2015-destructuring',
-            babelPlugin
-        ]
-    });
-    return result;
+    // TODO: Generate the task...
+    let il = new IL(code);
+    transformToIL(b, ast.program, il);
+    let newSrc = il.compile();
+    let result = newSrc;
+    return <any> result; // TODO: !!! not a task !!!
 }
+
+
+
+
+
+// TODO: doc... define a babel plugin that collects all scope info
+let augmentNodesWithScopeInfo = () => ({
+    visitor: <Visitor> {
+        // Collect info for all block-scopes that have their own bindings.
+        // TODO: What introduces a new name?
+        // - var decl (var, let, const)
+        // - func decl (hoisted)
+        // - class decl (not hoisted)
+        // - import decl (hoisted)
+        // - func expr. scope of name is only *inside* the func body
+        // - class expr. scope of name is only *inside* the class body
+        // - catch clause. scope of name is only the catch clause body
+        Block(path) {
+            if (path.scope.block !== path.node) return;
+            let idNames = Object.keys(path.scope.bindings);
+            let idKinds = idNames.map(name => path.scope.bindings[name].kind);
+            let scope = idNames.reduce((ids, name, i) => (ids[name] = idKinds[i], ids), {});
+            path.node.scope = scope;
+        }
+    }
+});
