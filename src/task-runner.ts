84a1c2d904ed3218f4e162352b8c8d52997f836c
Troy Gerwien <yortus@gmail.com>
2016-05-19 14:48:00 +0800

    improve runner

--- src/task-runner.ts
+++ src/task-runner.ts
@@ -12,38 +12,48 @@ export default class TaskRunner {
 
     // TODO: ...
     constructor(task: Task, globalObject?: {}) {
+        this.task = task;
         let vm = this.vm = makeVM();
         vm.ENV.value = globalObject || {};
         let code = this.code = recompile(task.code, vm);
     }
 
 
-    // TODO: ...
-    step() {
-        while (true) { // TODO: this loop doesn't belong here...
-            try {
-                this.code();
-                ++this.vm.PC.value;
+    // TODO: we are using exceptions for control flow in here. How awesome/insane is that? Non-rhetorical question...
+    // TODO: what is step() is called again after task finished/errored? Expected behaviour? Undefined behaviour for now...
+    step(): boolean|Error {
+        try {
+            this.code(); // NB: guaranteed to throw...
+        }
+        catch (err) {
+            let ex: Error = err; // workaround for TS1196 (see https://github.com/Microsoft/TypeScript/issues/8677)
+            if (ex instanceof Jump) {
+                // TODO: update the PC ready for the next call, and return to host...
+                this.vm.PC.value = ex.nextLine;
+                return false;
             }
-            catch (err) {
-                let ex: Error = err;
-                if (ex instanceof Branch) {
-                    this.vm.PC.value = ex.nextLine;
-                }
-                else if (ex instanceof Finish) {
-                    return;
-                }
-                else {
-                    // TODO: handle some other error...
-                    throw err;
-                }
+            else if (ex instanceof Done) {
+                // TODO: task completed without (uncaught) error...
+                return true;
+            }
+            else {
+                // TODO: we should probably handle and wrap non-Error errors that may crop up here (eg if task throws a string)
+                // TODO: uncaught error - surface it to the host...
+                return err;
             }
         }
     }
 
 
     // TODO: ...
-    private vm: VirtualMachine;
+    task: Task;
+
+
+    // TODO: ...
+    vm: VirtualMachine;
+
+
+    // TODO: ...
     private code: () => void;
 }
 
@@ -84,11 +94,11 @@ function makeVM() {
         LT:     (tgt, lhs, rhs) => tgt.value = lhs.value < rhs.value,
         NE:     (tgt, lhs, rhs) => tgt.value = lhs.value !== rhs.value,
 
-        B:      (line) => jump(line),
-        BF:     (line, arg) => arg.value ? null : jump(line),
-        BT:     (line, arg) => arg.value ? jump(line) : null,
+        B:      (line) => jumpTo(line),
+        BF:     (line, arg) => arg.value ? null : jumpTo(line),
+        BT:     (line, arg) => arg.value ? jumpTo(line) : null,
         CALL:   (tgt, func, thís, args) => tgt.value = func.value.apply(thís.value, args.value),
-        QUIT:   () => { throw new Finish(); },
+        QUIT:   () => { throw new Done(); },
 
         NEWARR: (tgt) => tgt.value = [],
         NEWOBJ: (tgt) => tgt.value = {},
@@ -105,10 +115,21 @@ function makeVM() {
         $7:     new Register('$7')
     };
 
-    function jump(line: number) {
+    function jumpTo(line: number) {
         // TODO: scope enter/exit, finally blocks
-        throw new Branch(line);
+        throw new Jump(line);
     }
+
+    // Inject prolog/epilog into all methods
+    Object.keys(vm).forEach(key => {
+        let oldMethod = vm[key];
+        if (typeof oldMethod !== 'function') return;
+        let newMethod = (...args) => {
+            // TODO: prolog (none for now)
+            oldMethod(...args);
+            throw new Jump(vm.PC.value + 1);
+        }
+    });
 
     return vm;
 }
@@ -118,7 +139,7 @@ function makeVM() {
 
 
 // TODO: ...
-class Branch extends Error {
+class Jump extends Error {
     constructor(public nextLine: number) { super(); }
 }
-class Finish extends Error { }
+class Done extends Error { }
