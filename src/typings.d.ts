fde0efb8dabd961cc6af21058660d65a6f822f2d
yortus <yortus@gmail.com>
2016-06-17 13:35:47 +0800

    no more triple-slash refs

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,6 +1,3 @@
-// Ambients still need triple-slash references... See https://github.com/Microsoft/TypeScript/issues/9208
-///<reference path="../node_modules/@types/mocha/index.d.ts" />
-///<reference path="../node_modules/@types/node/index.d.ts" />
 
 
 
