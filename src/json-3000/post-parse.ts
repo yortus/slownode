d3ecae64754daaebecf96936be7bebe424ee769b
yortus <yortus@gmail.com>
2016-06-17 10:27:03 +0800

    JSON3000#parse: full code comments

--- src/json-3000/post-parse.ts
+++ src/json-3000/post-parse.ts
@@ -20,8 +20,7 @@ export default function postParse(value: Serializable, reviver: Reviver): any {
 
     // TODO: backpatch refs to defs
     pathRefs.forEach(ref => {
-        let def = pathDefs.get(ref.path);
-        ref.obj[ref.key] = def;
+        ref.obj[ref.key] = pathDefs.get(ref.path);
     });
 
     // TODO: ...
@@ -30,34 +29,37 @@ export default function postParse(value: Serializable, reviver: Reviver): any {
     // TODO: ...
     function traverse(obj: Serializable, key: string, val: Serializable, path: string[]): void {
 
-        // TODO: special case  {$type: 'ref', ...}...
+        // Check if `val` represents a reference to a canonical decoding elsewhere in the object graph. If so,
+        // just record the details for backpatching after the full traversal has completed. Since there are no
+        // guarantees with property enumeration order, it would be unsafe to resolve the reference now, because
+        // the canonical path is refers to may not have been decoded yet.
         if (isReference(val)) {
             pathRefs.push({obj, key, path: val.path});
             return;
         }
 
-        let noRevive = false;
-
-        // TODO: recursive case... go depth first through props...
+        // If `val` is an object or array, then its properties are traversed (recursively) *before* `val` itself is
+        // put through the reviver. This ensures revivers are called in the reverse order of replacers.
         if (isSerializableObject(val)) {
 
-            // TODO: The escaped object was a POJO untouched by the replacer, so it won't be put through the reviver (but its props will)...
+            // Check for the special case of an 'escaped' plain object. The serializer only produces these encodings for
+            // plain objects that were untouched by all replacers, and which contain a '$type' property. Escaped objects
+            // decode to their unescaped value, without revival. However, the object's properties are still traversed.
             if (isEscaped(val)) {
                 obj[key] = val = val.raw;
-                noRevive = true;
+                var noRevive = true;
             }
 
-            // TODO: ...
+            // Traverse the properties of the object/array, so that they are recursively revived.
             Object.keys(val).forEach(subkey => {
-                let pathSegment = JSON.stringify(subkey).slice(1, -1).replace(/\./g, '\\u002e');
                 traverse(val, subkey, val[subkey], path.concat(encodePathSegment(subkey)));
             });
         }
 
-        // TODO: finally top level...
-        if (!noRevive) {
-            obj[key] = val = reviver.call(obj, key, val);
-        }
+        // Finally, run the reviver function on `val` itself (unless it was an escaped form). The revived value is
+        // the canonical value corresponding to the current path, so we also record that mapping here. These
+        // canonical mappings are used to resolve all the 'ref' references after traversal has completed.
+        if (!noRevive) obj[key] = val = reviver.call(obj, key, val);
         pathDefs.set(path.join('.'), val);
     }
 }
