eef6ff372fe3f5aa688a04ec7d1ede8bca4804a0
yortus <yortus@gmail.com>
2016-07-10 11:33:36 +0800

    move parser combinators to own file

--- src/serializers/kvon/parse/index.ts
+++ src/serializers/kvon/parse/index.ts
@@ -1,81 +1,7 @@
 import Reviver from './reviver';
 export {Reviver};
-
-
-
-
-
-// Parser Combinators
-interface Source {
-    text: string;
-    len: number;
-    pos: number;
-}
-interface Parser {
-    (src: Source): boolean;
-    <T>(src: Source, post: (isMatch: boolean, startPos: number) => T): T;
-}
-const STD_POST = (isMatch: boolean, startPos: number) => isMatch;
-function series(...exprs: Parser[]): Parser {
-    return (src, post = STD_POST) => {
-        let startPos = src.pos;
-        if (exprs.every(expr => expr(src))) return post(true, startPos);
-        src.pos = startPos;
-        return post(false, startPos);
-    };
-}
-function choice(...exprs: Parser[]): Parser {
-    return (src, post = STD_POST) => {
-        let startPos = src.pos;
-        let result = exprs.some(expr => expr(src));
-        return post(result, startPos);
-    }
-}
-function option(expr: Parser): Parser {
-    return (src, post = STD_POST) => {
-        let startPos = src.pos;
-        expr(src);
-        return post(true, startPos);
-    };
-}
-function zeroOrMore(expr: Parser): Parser {
-    return (src, post = STD_POST) => {
-        let startPos = src.pos;
-        while (expr(src)) { }
-        return post(true, startPos);
-    }
-}
-function oneOrMore(expr: Parser): Parser {
-    return series(expr, zeroOrMore(expr));
-}
-function not(expr: Parser): Parser {
-    return (src, post = STD_POST) => {
-        let startPos = src.pos;
-        let result = !expr(src);
-        src.pos = startPos;
-        return post(result, startPos);
-    }
-}
-function char(lo?: string, hi?: string): Parser {
-    let anyChar = arguments.length === 0;
-    hi = hi || lo;
-    return (src, post = STD_POST) => {
-        if (src.pos >= src.len) return post(false, src.pos);
-        if (anyChar) return post(true, src.pos++);
-        if (src.text[src.pos] < lo || src.text[src.pos] > hi) return post(false, src.pos);
-        return post(true, src.pos++);
-    };
-}
-function expect(expected: string, expr: Parser): Parser {
-    return (src, post = STD_POST) => {
-        let startPos = src.pos;
-        if (expr(src)) return post(true, startPos);
-        let before = (src.pos > 10 ? '...' : '') + src.text.slice(src.pos - 10, src.pos);
-        let after = src.text.slice(src.pos + 1, src.pos + 11) + (src.len - src.pos > 11 ? '...' : '');
-        let indicator = `${before}-->${src.text[src.pos]}<--${after}`;
-        throw new Error(`KVON: expected ${expected} but found '${src.text[src.pos]}': "${indicator}"`);
-    }
-}
+import {series, choice, option, zeroOrMore, oneOrMore, not, char, expect} from './parser-combinators';
+import {Source} from './parser-combinators';
 
 
 
@@ -195,7 +121,7 @@ export default function parse(text: string, reviver?: Reviver): {} {
     );
 
     // TODO: explain why this one is a function unlike the others (support circular refs)...
-    function value(src: Source, post = STD_POST): boolean {
+    function value(src: Source, post?): boolean {
         let rule = value['rule'] || (value['rule'] = choice(
             NULL,
             TRUE,
---
+++ src/serializers/kvon/parse/parser-combinators.ts
@@ -0,0 +1,76 @@
+
+
+
+
+
+// Parser Combinators
+export interface Source {
+    text: string;
+    len: number;
+    pos: number;
+}
+export interface Parser {
+    (src: Source): boolean;
+    <T>(src: Source, post: (isMatch: boolean, startPos: number) => T): T;
+}
+export const STD_POST = (isMatch: boolean, startPos: number) => isMatch;
+export function series(...exprs: Parser[]): Parser {
+    return (src, post = STD_POST) => {
+        let startPos = src.pos;
+        if (exprs.every(expr => expr(src))) return post(true, startPos);
+        src.pos = startPos;
+        return post(false, startPos);
+    };
+}
+export function choice(...exprs: Parser[]): Parser {
+    return (src, post = STD_POST) => {
+        let startPos = src.pos;
+        let result = exprs.some(expr => expr(src));
+        return post(result, startPos);
+    }
+}
+export function option(expr: Parser): Parser {
+    return (src, post = STD_POST) => {
+        let startPos = src.pos;
+        expr(src);
+        return post(true, startPos);
+    };
+}
+export function zeroOrMore(expr: Parser): Parser {
+    return (src, post = STD_POST) => {
+        let startPos = src.pos;
+        while (expr(src)) { }
+        return post(true, startPos);
+    }
+}
+export function oneOrMore(expr: Parser): Parser {
+    return series(expr, zeroOrMore(expr));
+}
+export function not(expr: Parser): Parser {
+    return (src, post = STD_POST) => {
+        let startPos = src.pos;
+        let result = !expr(src);
+        src.pos = startPos;
+        return post(result, startPos);
+    }
+}
+export function char(lo?: string, hi?: string): Parser {
+    let anyChar = arguments.length === 0;
+    hi = hi || lo;
+    return (src, post = STD_POST) => {
+        if (src.pos >= src.len) return post(false, src.pos);
+        if (anyChar) return post(true, src.pos++);
+        if (src.text[src.pos] < lo || src.text[src.pos] > hi) return post(false, src.pos);
+        return post(true, src.pos++);
+    };
+}
+export function expect(expected: string, expr: Parser): Parser {
+    return (src, post = STD_POST) => {
+        let startPos = src.pos;
+        if (expr(src)) return post(true, startPos);
+        let before = (src.pos > 10 ? '...' : '') + src.text.slice(src.pos - 10, src.pos);
+        let after = src.text.slice(src.pos + 1, src.pos + 11) + (src.len - src.pos > 11 ? '...' : '');
+        let indicator = `${before}-->${src.text[src.pos]}<--${after}`;
+        throw new Error(`KVON: expected ${expected} but found '${src.text[src.pos]}': "${indicator}"`);
+    }
+}
