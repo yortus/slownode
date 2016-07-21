ed9bd638fcf052c3ed1aa7011ff09d9b777c151c
Troy Gerwien <yortus@gmail.com>
2016-07-21 13:49:16 +0800

    prep for Epoch testing
    ...disabled all other unit tests temporarily

--- src/epoch/epoch.ts
+++ src/epoch/epoch.ts
@@ -19,52 +19,39 @@ export default class Epoch extends EventEmitter {
         super();
 
         // Validate options
+        // TODO: allow setting path as option
         this._dirname = path.join(process.cwd(), 'slowfiles');
 
-        // TODO: create dir if not exists...
+        // TODO: create dir if it does not exist...
         try { fs.mkdirSync(this._dirname); }
         catch (err) { }
 
         // TODO: load/revive running scripts from storage...
-        this._scriptIds = [];
         let filenames = fs.readdirSync(this._dirname);
         filenames.forEach(filename => {
 
             // TODO: resume script...
-            let scriptId = filename.slice(0, -path.extname(filename).length);
-            this._scriptIds.push(scriptId);
             filename = path.join(this._dirname, filename);
             let snapshot = fs.readFileSync(filename, 'utf8');
             let script = Script.fromSnapshot(snapshot);
-
             this.runToCompletion(script, filename);
         });
     }
 
 
     // TODO: ... fix signature - accept scriptIdHint and return actualScriptId (unique in epoch)
-    eval(source: string, scriptId?: string) {
-
-        // TODO: ensure script ID is a valid filename...
-        // TODO: better to just separate scriptId from filename, and use a GUID for filename? Less discoverable on disk...
-        scriptId = scriptId || '«unidentified script»';
-        scriptId = scriptId.replace(/[^a-zA-Z0-9_«»] /, '_');
-
-        // TODO: ensure script ID is unique within epoch...
-        let index = 0;
-        while (true) {
-            let sid = `${scriptId}${index++ || ''}`;
-            if (this._scriptIds.indexOf(sid) !== -1) continue;
-            scriptId = sid;
-            break;
-        }
+    eval(source: string, scriptName?: string) {
 
         // TODO: create the script object...
-        let script = new Script(source, {name: scriptId});
-        this._scriptIds.push(scriptId);
+        scriptName = scriptName || '«unnamed script»';
+        let script = new Script(source, {name: scriptName});
+
+        // Derive a valid filename from the script name.
+        // TODO: better to just use a GUID for filename? Less discoverable as to what's what on disk...
+        let filename = scriptName.replace(/[^a-zA-Z0-9_«»] /, '_');
 
         // TODO: Save the script's initial snapshot to disk...
-        let filename = path.join(this._dirname, scriptId + '.slow');
+        filename = path.join(this._dirname, filename + '.slow');
         fs.writeFileSync(filename, script.snapshot(), {encoding: 'utf8'});
 
         // TODO: run the script...
@@ -73,29 +60,18 @@ export default class Epoch extends EventEmitter {
 
 
     // TODO: ...
-    private _dirname: string;
-
-
-    // TODO: ...
-    private _scriptIds: string[];
-
-
-    // TODO: ...
     private runToCompletion(script: Script, filename: string) {
-        let scriptId = script.name;
 
         // TODO: Kick off script using an IIAFE...
-        // TODO: do we need to keep a reference to the script/jasm/interpreter/progress after this? Why? Why not?
         (async () => {
             // TODO: run to completion...
             try {
-                // TODO: saving at await points...
                 for (let step of script) {
                     await step;
 
+                    // TODO: ...
                     let instr = script.program.lines[script.registers.get('PC')];
                     let shouldPark = instr.type === 'instruction' && instr.opcode.toUpperCase() === 'AWAIT';
-
                     if (shouldPark) {
                         let snapshot = script.snapshot();
                         fs.writeFileSync(filename, snapshot, {encoding: 'utf8'});
@@ -110,17 +86,20 @@ export default class Epoch extends EventEmitter {
                 }
 
                 // TODO: script finished successfully... emit something?
-                this.emit('end', scriptId);
+                this.emit('end', script);
             }
             catch (err) {
-                this.emit('error', err, scriptId);
+                this.emit('error', err, script);
             }
             finally {
 
                 // TODO: script finished...
-                this._scriptIds.splice(this._scriptIds.indexOf(scriptId), 1);
                 fs.unlinkSync(filename);
             }
         })();
     }
+
+
+    // TODO: ...
+    private _dirname: string;
 }
--- test/script/resuming-a-script-from-a-snapshot.ts
+++ test/script/resuming-a-script-from-a-snapshot.ts
@@ -1,76 +1,76 @@
-// TODO: add another test file for testing snapshots...
-import {Script, KVON} from 'slownode';
-import {expect} from 'chai';
+// // TODO: add another test file for testing snapshots...
+// import {Script, KVON} from 'slownode';
+// import {expect} from 'chai';
 
 
 
 
 
-describe('Resuming a script from a snapshot', () => {
+// describe('Resuming a script from a snapshot', () => {
 
-    let tests = [
-        `let result = 42;                                           ==> 42`,
-        `let result = 1 - 2 * (3 + 4 / 5);                          ==> -6.6`,
-        `let result = -1 / 0;                                       ==> -Infinity`,
-        `let result = 2 - '1';                                      ==> ERROR: Static checking failed...`,
-        `let s: any = 'foo', result = s * 3;                        ==> NaN`,
-        `let one: any = '1', result = 2 + one;                      ==> "21"`,
-        `let one: any = '1', result = 2 - one;                      ==> 1`,
-        'let s = "bar", result = `foo${s}baz${[1, 2, 3]}`;          ==> "foobarbaz1,2,3"',
-        `let n = 2; while (n < 1000) n = n*n; let result = n;       ==> 65536`,
-        `undeclaredVar = 1;                                         ==> ERROR: Static checking failed...`,
-        `const uninitialized;                                       ==> ERROR: Static checking failed...`,
-        `const t = undeclaredVar;                                   ==> ERROR: Static checking failed...`,
-        `let obj:any = {}, ref = obj.bar.baz;                       ==> ERROR: Cannot read property 'baz'...`,
-        // TODO: add some 'unsnapshotable' things for completeness (eg Promise instances)
-    ];
+//     let tests = [
+//         `let result = 42;                                           ==> 42`,
+//         `let result = 1 - 2 * (3 + 4 / 5);                          ==> -6.6`,
+//         `let result = -1 / 0;                                       ==> -Infinity`,
+//         `let result = 2 - '1';                                      ==> ERROR: Static checking failed...`,
+//         `let s: any = 'foo', result = s * 3;                        ==> NaN`,
+//         `let one: any = '1', result = 2 + one;                      ==> "21"`,
+//         `let one: any = '1', result = 2 - one;                      ==> 1`,
+//         'let s = "bar", result = `foo${s}baz${[1, 2, 3]}`;          ==> "foobarbaz1,2,3"',
+//         `let n = 2; while (n < 1000) n = n*n; let result = n;       ==> 65536`,
+//         `undeclaredVar = 1;                                         ==> ERROR: Static checking failed...`,
+//         `const uninitialized;                                       ==> ERROR: Static checking failed...`,
+//         `const t = undeclaredVar;                                   ==> ERROR: Static checking failed...`,
+//         `let obj:any = {}, ref = obj.bar.baz;                       ==> ERROR: Cannot read property 'baz'...`,
+//         // TODO: add some 'unsnapshotable' things for completeness (eg Promise instances)
+//     ];
 
-    tests.forEach(test => {
-        let source = test.split('==>')[0].trim();
-        let result = test.split('==>')[1].trim();
-        let name = source.replace(/[\r\n]+/g, ' ');
-        name = name.length > 40 ? name.slice(0, 37) + '...' : name;
+//     tests.forEach(test => {
+//         let source = test.split('==>')[0].trim();
+//         let result = test.split('==>')[1].trim();
+//         let name = source.replace(/[\r\n]+/g, ' ');
+//         name = name.length > 40 ? name.slice(0, 37) + '...' : name;
 
-        it(name, nodeify(async () => {
-            let expectedError = result.startsWith('ERROR');
-            let expected: any = expectedError ? result : eval(result);
-            let actual;
+//         it(name, nodeify(async () => {
+//             let expectedError = result.startsWith('ERROR');
+//             let expected: any = expectedError ? result : eval(result);
+//             let actual;
 
-            try {
-                let script = new Script(source);
-                let stepsDone = 0;
-                while (true) {
-                    let step = script.next();
-                    if (step.done) break;
-                    await step.value;
-                    ++stepsDone;
+//             try {
+//                 let script = new Script(source);
+//                 let stepsDone = 0;
+//                 while (true) {
+//                     let step = script.next();
+//                     if (step.done) break;
+//                     await step.value;
+//                     ++stepsDone;
 
-                    // After every five steps, take a snapshot of the running script, destroy the
-                    // script, and then replace the script with a new one created from the snapshot.
-                    if (stepsDone % 5 === 0) {
-                        let snapshot = script.snapshot();
-                        script = null;
-                        script = Script.fromSnapshot(snapshot);
-                    }
-                }
-                actual = script.registers.get('ENV')['result'];
-            }
-            catch (er) {
-                actual = `ERROR: ${er.message}`;
-                if (expectedError && expected.endsWith('...')) {
-                    actual = actual.slice(0, expected.length - 3) + '...';
-                }
-            }
-            expect(actual).to.deep.equal(expected);
-        }));
-    });
-});
-
-
+//                     // After every five steps, take a snapshot of the running script, destroy the
+//                     // script, and then replace the script with a new one created from the snapshot.
+//                     if (stepsDone % 5 === 0) {
+//                         let snapshot = script.snapshot();
+//                         script = null;
+//                         script = Script.fromSnapshot(snapshot);
+//                     }
+//                 }
+//                 actual = script.registers.get('ENV')['result'];
+//             }
+//             catch (er) {
+//                 actual = `ERROR: ${er.message}`;
+//                 if (expectedError && expected.endsWith('...')) {
+//                     actual = actual.slice(0, expected.length - 3) + '...';
+//                 }
+//             }
+//             expect(actual).to.deep.equal(expected);
+//         }));
+//     });
+// });
 
 
 
-// TODO: copypasta from ../epoch.ts... put this somewhere for all to share...
-function nodeify(fn: () => Promise<any>): (cb: (error?: any) => void) => void {
-    return done => { fn().then(() => done(), done) };
-}
+
+
+// // TODO: copypasta from ../epoch.ts... put this somewhere for all to share...
+// function nodeify(fn: () => Promise<any>): (cb: (error?: any) => void) => void {
+//     return done => { fn().then(() => done(), done) };
+// }
