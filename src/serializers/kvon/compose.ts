e96a36b4ce0a05d612a673e1532f171f3ffd3510
Troy Gerwien <yortus@gmail.com>
2016-07-25 14:36:04 +0800

    document KVON#compose

--- src/serializers/kvon/compose.ts
+++ src/serializers/kvon/compose.ts
@@ -5,8 +5,19 @@ import Reviver from './reviver';
 
 
 
-// TODO: ...
+/**
+ * Create a composite replacer that is equivalent to calling each of the given
+ * replacers in sequence until one of them returns a modified (i.e. replaced)
+ * value. If *none* of the constituent replacers returns a modified value,
+ * then the composite replacer returns its value unmodified.
+ */
 export default function compose(...replacers: Replacer[]): Replacer;
+/**
+ * Create a composite reviver that is equivalent to calling each of the given
+ * revivers in sequence until one of them returns a modified (i.e. revived)
+ * value. If *none* of the constituent revivers returns a modified value,
+ * then the composite reviver returns its value unmodified.
+ */
 export default function compose(...revivers: Reviver[]): Reviver;
 export default function compose(...transformers: Array<(this: {}, key: string, val: {}) => any>) {
     return function composed(this: {}, key: string, val: {}) {
